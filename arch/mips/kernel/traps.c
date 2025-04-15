10f836965d31c95f95ee2ab0d9e76314531c0e0a
gtxaspec <gtxent@gmail.com>
2025-04-14 18:31:10 -0700

    Revert "fix array bounds errors in exception table handling and array comparisons"
    This reverts commit 629d9cf1abb7adccf26048bc2f3ce0708d112c20.

--- arch/mips/kernel/traps.c
+++ arch/mips/kernel/traps.c
@@ -428,9 +428,7 @@ static const struct exception_table_entry *search_dbe_tables(unsigned long addr)
 {
 	const struct exception_table_entry *e;
 
-	// bad ! e = search_extable(__start___dbe_table, __stop___dbe_table - 1, addr);
-	e = search_extable(__start___dbe_table, __stop___dbe_table, addr);
-
+	e = search_extable(__start___dbe_table, __stop___dbe_table - 1, addr);
 	if (!e)
 		e = search_module_dbetables(addr);
 	return e;
